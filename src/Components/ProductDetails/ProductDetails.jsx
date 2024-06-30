b8b39c7c712b9539b8a1d8f940307fd0cda9344f
abdalrhman <abdooamino0@gmail.com>
2024-07-01 00:56:31 +0300

    test

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -119,7 +119,7 @@ export default function ProductDetails() {
                         </Slider>
                     </div>
                     <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                        <h2 className="text-sm title-font text-gray-500 tracking-widest">{data?.brand.name}</h2>
+                        <h2 className="text-sm title-font text-gray-500 tracking-widest">{data?.brand?.name}</h2>
                         <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{data?.title}</h1>
                         <div className="flex mb-4">
                             <span className="flex items-center">
@@ -166,22 +166,6 @@ export default function ProductDetails() {
                                 <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none" />
                                 <button className="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none" />
                             </div>
-                            {/* <div className="flex ml-6 items-center">
-                                <span className="mr-3">Size</span>
-                                <div className="relative">
-                                    <select className="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
-                                        <option>SM</option>
-                                        <option>M</option>
-                                        <option>L</option>
-                                        <option>XL</option>
-                                    </select>
-                                    <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4" viewBox="0 0 24 24">
-                                            <path d="M6 9l6 6 6-6" />
-                                        </svg>
-                                    </span>
-                                </div>
-                            </div> */}
                         </div>
                         <div className="flex">
                             <span className="title-font font-medium text-2xl text-gray-900">{data?.price} EGP</span>
